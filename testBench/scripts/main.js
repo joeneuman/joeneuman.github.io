a171b896363cb6c03399278c1e8c9b348349e27f
Joe Neuman <joeneuman@mystg.com>
2015-11-03 17:07:59 -0700

    start

---
+++ testBench/scripts/main.js
@@ -0,0 +1,14 @@
+var motorBike = {
+
+  // Only change code below this line.
+ "wheels": 3,
+ "engines": 0,
+ "seats": 1 
+
+
+  // Only change code above this line.
+
+};
+motorBike.wheels = 4;
+motorBike.engines = 1;
+motorBike.seats = 4;
