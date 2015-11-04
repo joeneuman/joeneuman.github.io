3f52f530dbfbc575bd6cf3560916bb8a03fe7d85
Joe Neuman <joeneuman@mystg.com>
2015-11-04 16:47:53 -0700

    3 letters

--- testBench/scripts/main.js
+++ testBench/scripts/main.js
@@ -2,9 +2,6 @@ var first = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o
 var second = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var theird = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var threeLetter = [];
-if (threeLetter === 49) {
-  
-  break;
 
 for (var i = 0; i < first.length; i++) {
   for (var j = 0; j < second.length; j++) {
