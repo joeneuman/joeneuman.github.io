f1676799f61c08fa0ecb8592ede75385565de475
Joe Neuman <joeneuman@mystg.com>
2015-11-06 17:31:09 -0700

    "updates"

--- testBench/scripts/main.js
+++ testBench/scripts/main.js
@@ -1,13 +1,70 @@
-var first = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var second = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var theird = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var threeLetter = [];
-
-for (var i = 0; i < first.length; i++) {
-  for (var j = 0; j < second.length; j++) {
-    for (var k = 0; k < theird.length; k++) {
-    document.write(first[i] + second[j] + theird[k] + ".io<br>");
-     
-    }
+for (var i = 0; i < animals.length; i++) {
+  for (var j = 0; j < products.length; j++) {
+    foodItems[k] = animals[i] + products[j];
+    
   }
 }
+for (var i = 0; i < animals.length; i++) {
+  for (var j = 0; j < products.length; j++) {
+    foodItems[k] = animals[i] + products[j];
+    k++;
+  }
+} 
+
+  for (var j = 0; j < 10; j++) {
+  }
+  
+  
+  // Code the first line of an if statement that tests whether a segment with an index represented by indexNum exists in a string.
+  
+  if (indexNum !== -1) { 
+
+// 11) Code an alert that displays the index number of the first character of a segment represented by a variable that occurs within a string represented by a second variable. 
+    
+    alert(secondVar.indexOf(be));
+
+// 12) Code the first line of an if statement that tests whether a 3-character slice at the beginning of a string represented by one variable can be found somewhere within a string represented by a second variable.
+
+if (var2.indexOf(var1.slice(0, 3)) !== -1) { 
+  
+var indx = text.indexOf(seg);
+
+
+
+11)
+if (vrS.charAt(2) === "a") {
+if (str.charAt(2) === "a") {
+  
+var x = str.charAt(0);
+   
+
+var indx = text.indexOf("seg");
+
+// 15)
+var indx = text.lastIndexOf("seg");
+
+if (indexNum !== -1) {
+  
+alert(stringV.indexOf(seg));
+
+if (var2.indexOf(var1.slice(0, 3)) !== -1) {
+  
+for (var i = 0; i < array.length; i++) {
+  var element = array[i];
+  
+}
+
+
+for (var i = 0; i < text.length; i++) {
+ textArr[i] = text.charAt(i);
+}
+for (var i = 0; i < text.length; i++) {
+  textArray[i] = text.charAt(i);
+  
+  
+} 
+  
+var comOfVar = striVar.charAt(0, 1) + anoString.charAt(anoString - 1)
+
+var combo = str1.charAt(0) + str2.charAt(str2.length - 1); 
+
